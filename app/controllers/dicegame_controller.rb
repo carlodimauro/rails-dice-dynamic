bbc75652bfad57fa35fb2a5c15242085417eb146
carlodimauro <146589712+carlodimauro@users.noreply.github.com>
2023-10-30 20:45:07 +0000

    working

--- app/controllers/dicegame_controller.rb
+++ app/controllers/dicegame_controller.rb
@@ -5,6 +5,16 @@ class DicegameController < ApplicationController
   end
 
   def roll
+
+    @die = params.fetch("no_of_d")
+    @sides = params.fetch("no_of_s")
+
+    @rollresult = []
+
+      @die.to_i.times do
+      @rollresult.push(rand(1..@sides.to_i))
+      end
+
     render({ :template => "/dicetemplates/rolldice"})
   end
 
